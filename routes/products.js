8573b9e9b0a5db96d714e33692cdf66d178ca760
Eicot <zha232zha@gmail.com>
2023-05-03 18:09:50 +0800

    Step 8
    Create a new product

--- routes/products.js
+++ routes/products.js
@@ -3,12 +3,41 @@ const router = express.Router();
 
 // #1 import in the Product model
 const {Product} = require('../models')
+// import in the Forms
+const { bootstrapField, createProductForm } = require('../forms');
 
 router.get('/', async (req,res)=>{
     // #2 - fetch all the products (ie, SELECT * from products)
     let products = await Product.collection().fetch();
     res.render('products/index', {
         'products': products.toJSON() // #3 - convert collection to JSON
+    })
+})
+
+router.get('/create', async (req, res) => {
+    const productForm = createProductForm();
+    res.render('products/create',{
+        'form': productForm.toHTML(bootstrapField)
+    })
+})
+
+router.post('/create', async(req,res)=>{
+    console.log('post/create')
+    const productForm = createProductForm();
+    productForm.handle(req, {
+        'success': async (form) => {
+            const product = new Product();
+            product.set('name', form.data.name);
+            product.set('cost', form.data.cost);
+            product.set('description', form.data.description);
+            await product.save();
+            res.redirect('/products');
+        },
+        'error': async (form) => {
+            res.render('products/create',{
+                'form': form.toHTML(bootstrapField)
+            })
+        }
     })
 })
 
